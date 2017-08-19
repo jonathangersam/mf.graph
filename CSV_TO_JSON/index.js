0b9d51e4bb7a06336ba9ade768a2b46527db6c95
jonathangersam <jonathangersam@gmail.com>
2017-08-19 11:32:07 +0800

    csv-to-json converter working

--- CSV_TO_JSON/index.js
+++ CSV_TO_JSON/index.js
@@ -1,20 +1,17 @@
-//require the csvtojson converter class
-var Converter = require("csvtojson").Converter;
-// create a new converter object
-var converter = new Converter({});
+const PATH = "./tmp/data.csv";
 
-// call the fromFile function which takes in the path to your
-// csv file as well as a callback function
-converter.fromFile("./path-to-your-file.csv",function(err,result){
-    // if an error has occured then handle it
-    if(err){
-        console.log("An Error Has Occured");
-        console.log(err);
-    }
-    // create a variable called json and store
-    // the result of the conversion
-    var json = result;
 
-    // log our json to verify it has worked
-    console.log(json);
+/* BOOTSTRAP */
+getJsonFromCsv(PATH, function(d) {
+    console.log(d);
 });
+
+/* FP Functions */
+function getJsonFromCsv(path, callback){
+    const csv=require('csvtojson');
+    let data = [];
+    csv().fromFile(path)
+        .on('json', d => {data.push(d)})
+        .on('done', () => {callback(data)});
+}
+
